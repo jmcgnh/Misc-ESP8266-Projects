4e00316d9951cdbff016ceda957f3d5f8d8f0eeb
Jim McGinness <jmcgnh@gmail.com>
2019-11-07 15:17:37 -0500

    New arduino.php and deploy.pl scripts to go with new approach for OTA via website. Sketch OTA_config_draft1.ino modified to match.

---
+++ arduino.php
@@ -0,0 +1,99 @@
+<?PHP
+
+header('Content-type: text/plain; charset=utf8', true);
+
+function check_header($name, $value = false) {
+    if(!isset($_SERVER[$name])) {
+        return false;
+    }
+    if($value && $_SERVER[$name] != $value) {
+        return false;
+    }
+    return true;
+}
+
+function sendFile($path) {
+    header($_SERVER["SERVER_PROTOCOL"].' 200 OK', true, 200);
+    header('Content-Type: application/octet-stream', true);
+    header('Content-Disposition: attachment; filename='.basename($path));
+    header('Content-Length: '.filesize($path), true);
+    header('x-MD5: '.md5_file($path), true);
+    readfile($path);
+}
+
+if(!check_header('HTTP_USER_AGENT', 'ESP8266-http-Update')) {
+    header($_SERVER["SERVER_PROTOCOL"].' 403 Forbidden', true, 403);
+    echo "only for ESP8266 updater!\n";
+    exit();
+}
+
+if(
+    !check_header('HTTP_X_ESP8266_STA_MAC') ||
+    !check_header('HTTP_X_ESP8266_AP_MAC') ||
+    !check_header('HTTP_X_ESP8266_FREE_SPACE') ||
+    !check_header('HTTP_X_ESP8266_SKETCH_SIZE') ||
+    !check_header('HTTP_X_ESP8266_CHIP_SIZE') ||
+    !check_header('HTTP_X_ESP8266_SDK_VERSION') ||
+    !check_header('HTTP_X_ESP8266_VERSION')
+) {
+    header($_SERVER["SERVER_PROTOCOL"].' 403 Forbidden', true, 403);
+    echo "only for ESP8266 updater! (header)\n";
+    exit();
+}
+
+$macaddr = '';
+if(isset($_SERVER['HTTP_X_ESP8266_STA_MAC'])) {
+    $macaddr = $_SERVER['HTTP_X_ESP8266_STA_MAC'];
+}
+
+$model = 'unknown';
+
+$fp2 = fopen( 'debug_modeldb.txt', 'w');
+$fp = fopen( 'modeldb.csv', 'r');
+while( ($data = fgetcsv( $fp, 1024, "\t")) != FALSE) {
+   fprintf( $fp2, "data row = '%s'\n", serialize( $data));
+   if( strcmp( $data[0], $macaddr )) {
+      continue;
+     } else {
+      $model = $data[1];
+      break;
+     }
+   }
+fclose( $fp);
+fclose( $fp2);
+
+$fp2 = fopen( 'model.txt', 'w');
+fprintf( $fp2, "model= %s\n", $model);
+fclose( $fp2);
+
+$fp = popen( "/bin/ls -rt model/$model | tee filelist.raw", 'r');
+$latestfile = trim( fgets( $fp));
+fclose( $fp);
+
+$filepath = "model/$model/$latestfile";
+
+$fp2 = fopen( 'filepath.txt', 'w');
+fprintf( $fp2, "filepath = %s\n", $filepath);
+fprintf( $fp2, "exists = %s\n", (file_exists( $filepath) ? "TRUE" : "FALSE" ));
+fclose( $fp2);
+
+$versionstring = '';
+
+if(isset($_SERVER['HTTP_X_ESP8266_VERSION'])) {
+    $versionstring = $_SERVER['HTTP_X_ESP8266_VERSION'] . '.bin';
+  }
+
+$fp2 = fopen( 'versionstring.txt', 'w');
+fprintf( $fp2, "versionstring = %s\n", $versionstring);
+fclose( $fp2);
+
+if( file_exists( $filepath )) {
+    if( strcmp( $latestfile, $versionstring) ) {
+        sendFile( $filepath);
+    } else {
+        header($_SERVER["SERVER_PROTOCOL"].' 304 Not Modified', true, 304);
+    }
+    exit(0); /* these are the only two cases that can be considered 'success' */
+}
+
+header($_SERVER["SERVER_PROTOCOL"].' 500 no version for ESP MAC', true, 500);
---
+++ deploy.pl
@@ -0,0 +1,43 @@
+#! /bin/env perl
+#
+# deploy binary to espsite
+#
+# 2019-11-07 -- jmcg
+# 
+
+use strict;
+use MIME::Base64;
+
+my $versionline = `grep "String versionstring" *.ino `;
+(my $versionstring) = $versionline =~ m{"(.*)"};
+
+printf STDERR "v='%s'\n", $versionstring;
+my $binary = `echo *.bin`;
+chomp $binary;
+printf STDERR "b='%s'\n", $binary;
+my $model = $binary;
+
+my $result = `scp $binary  jmcg\@jmcgnh041:/var/tmp/.` or "scp failed";
+printf STDERR "r1 = %s\n", $result;
+my $commands = <<"EOF";
+cp /var/tmp/$binary /var/www/espsite/update/model/$model/$versionstring.bin
+chown www-data:www-data /var/www/espsite/update/model/$model/$versionstring.bin
+EOF
+printf STDERR "-----\nc=%s\n-----\n", $commands;
+my $encommands = encode_base64( $commands);
+printf STDERR "e=%s", $encommands;
+
+open SCR, ">",  "./script";
+print SCR $encommands;
+
+$result = `scp ./script jmcg\@jmcgnh041:/var/tmp/.` or "scp2 failed";
+
+printf STDERR "r2=%s", $result;
+
+$result = `ssh jmcg\@jmcgnh041 'base64 -d --ignore-garbage </var/tmp/script | sudo bash -ex'` or "ssh failed";
+
+printf STDERR "r3=%s", $result;
+
+# can now send a signal to a device for it to check for a new version immediately insteal of waiting out the OTA_update_interval
+
+if( exits( $ENV{DEPLOYTARGET} ) { my $target = $ENV{DEPLOYTARGET}; `curl http://$target/` }
